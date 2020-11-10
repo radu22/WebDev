26be455a79fb253bef4aa32b284319a9b02cbdd0
Radu <catarama.radu22@gmail.com>
2020-11-10 15:36:38 +0200

    First commit

---
+++ full-width/finance/subscribe.php
@@ -0,0 +1,11 @@
+<?php
+	$to = "catarama.radu@yahoo.com"; // this is your Email address
+	$from  = $_POST['subsEmail']; // this is the sender's Email address
+
+	$subject = "New subscriber";
+	$message = "A new subscriber appeared " .  $from .;
+
+	$headers = 'From: ' . $from;
+	mail($to, $subject, $message, $headers);
+
+?>
